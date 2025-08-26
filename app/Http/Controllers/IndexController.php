a8fbfe173372a43c1964e69a8c69c33bfce77cc1
Alex <puyaalexxx@gmail.com>
2025-08-26 15:42:32 +0300

    added listing controller with vue componetns for pages

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -2,12 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Listing;
 use Inertia\Inertia;
 
 class IndexController extends Controller
 {
     public function index()
     {
+        $listings = Listing::all();
+
         return Inertia::render('Index/Index', [
             'message' => 'Hello World!'
         ]);
