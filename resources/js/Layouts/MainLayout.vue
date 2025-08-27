3e892907785672c2bfa574d087b17b79e3d26239
Alex <puyaalexxx@gmail.com>
2025-08-27 16:00:28 +0300

    added flash message on listing creation and passed to vue component

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -2,9 +2,27 @@
   <Link href="/">Index Page</Link>
   <Link href="/show">Show Page </Link>
 
+  <div v-if="flashSuccess" class="success">
+    {{ flashSuccess }}
+  </div>
+
   <slot> </slot>
 </template>
 
 <script setup>
-import { Link } from '@inertiajs/vue3';
+import { Link, usePage } from '@inertiajs/vue3';
+import { computed } from 'vue';
+
+//page.props.value.flash.success
+const page = usePage();
+
+const flashSuccess = computed(() => page.props.flash?.success);
+
+console.log(page.props);
 </script>
+
+<style scoped>
+.success {
+  background-color: green;
+}
+</style>
